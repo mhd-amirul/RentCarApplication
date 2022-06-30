974e6da97896eddc832ddb9bc12a06a7401afcff
Your Name <mhd-amirul>
2022-06-30 11:00:25 +0700

    fix login View

--- resources/views/authentikasi/login.blade.php
+++ resources/views/authentikasi/login.blade.php
@@ -2,38 +2,44 @@
 
 @section('container')
     <div class="row justify-content-center mt-5">
-        <div class="col-lg-5 mt-4">
+        <div class="col-lg-9 mt-4">
             <main class="form-signin mb-2">
-                <div class="d-flex justify-content-center mb-3">
-                    <img src="{{ url('images/person.png') }}" width="100" alt="">
-                </div>
-                <form action="login" method="post">
-                    @csrf
-                    <div class="form-floating">
-                        <input name="email" type="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="email" value="{{ old('email') }}" required>
-                        <label for="email">Email</label>
-                        @error('email')
-                            <div class="invalid-feedback">
-                                {{ $message }}
+                <div class="card px-3 py-3 border border-gray-800">
+                    <div class="row justify-content-center mb-5">
+                        <div class="col-sm-7">
+                            <div class="d-flex justify-content-center mb-3">
+                                <img src="{{ url('images/person.png') }}" width="100" alt="">
                             </div>
-                        @enderror
-                    </div>
-                    <div class="form-floating">
-                        <input name="password" type="password" class="form-control" id="password" placeholder="password" required>
-                        <label for="password">Password</label>
-                    </div>
-                    <button class="w-100 btn btn-lg btn-secondary" type="submit">Sign in</button>
-                </form>
-                <div class="row">
-                    <div class="col">
-                        <small class="d-block text-center mt-3">
-                            Forgot Password?<a href="{{ route('forgotPass') }}"> Click Here!</a>
-                        </small>
-                    </div>
-                    <div class="col">
-                        <small class="d-block text-center mt-3">
-                            Dont Have Account? <a href="register">Register Now!</a>
-                        </small>
+                            <form action="login" method="post">
+                                @csrf
+                                <div class="form-floating">
+                                    <input name="email" type="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="email" value="{{ old('email') }}" required>
+                                    <label for="email">Email</label>
+                                    @error('email')
+                                        <div class="invalid-feedback">
+                                            {{ $message }}
+                                        </div>
+                                    @enderror
+                                </div>
+                                <div class="form-floating">
+                                    <input name="password" type="password" class="form-control" id="password" placeholder="password" required>
+                                    <label for="password">Password</label>
+                                </div>
+                                <button class="w-100 btn btn-lg btn-secondary" type="submit">Sign in</button>
+                            </form>
+                            <div class="row">
+                                <div class="col">
+                                    <small class="d-block text-center mt-3">
+                                        Forgot Password?<a href="{{ route('forgotPass') }}"> Click Here!</a>
+                                    </small>
+                                </div>
+                                <div class="col">
+                                    <small class="d-block text-center mt-3">
+                                        Dont Have Account? <a href="register">Register Now!</a>
+                                    </small>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </main>
