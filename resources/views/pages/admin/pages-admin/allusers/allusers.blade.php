e80b2b5b6bae041732106790d75782d47b81c84a
Your Name <mhd-amirul>
2022-06-30 21:48:12 +0700

    fix allUser View

--- resources/views/pages/admin/pages-admin/allusers/allusers.blade.php
+++ resources/views/pages/admin/pages-admin/allusers/allusers.blade.php
@@ -72,13 +72,6 @@
 </div>
 </form>
 
-@if (session()->has('success'))
-<div class="alert alert-success alert-dismissible fade show" role="alert">
-    {{ session('success') }}
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-</div>
-@endif
-
 @if ($user->count())
 <div class="container">
     <div class="row">
