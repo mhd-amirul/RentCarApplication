3eca9e6c8de0b5bda6b84d7039eb57c8303f1c66
Your Name <mhd-amirul>
2022-06-30 16:00:22 +0700

    fix setLokshop View

--- resources/views/pages/rental/toko/setLocation.blade.php
+++ resources/views/pages/rental/toko/setLocation.blade.php
@@ -2,28 +2,34 @@
 
 @section('container')
 <div class="row justify-content-center">
-    <div class="col-12">
-        <main class="form-registration mt-3">
-                <div class="card-header bg-dark text-white mt-3 mb-3">
-                    Atur Lokasi Toko
-                </div>
-                <div class="card_body border border-dark">
-                    <div id='map' style='width: 100%; height: 60vh;'></div>  
-                </div>
-                <form action="{{ route('saveLocation', $shop->id) }}" method="post">
-                @method('put')
-                @csrf
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="form-floating">
-                            <input type="text" name="longitude" class="form-control rounded-top" id="longitude" placeholder="longitude" hidden>
-                            <input type="text" name="latitude" class="form-control rounded-top" id="latitude" placeholder="Latitude" hidden>
+    <div class="col-lg-12">
+        <div class="card px-5 pt-4 pb-5 border border-gray-800">
+            <main class="form-registration mt-3">
+                    <div class="card-header bg-dark text-white mt-3 mb-3">
+                        Map
+                    </div>
+                    <div class="card_body border border-dark">
+                        <div id='map' style='width: 100%; height: 60vh;'></div>
+                    </div>
+                    <form action="{{ route('saveLocation', $shop->id) }}" method="post">
+                    @method('put')
+                    @csrf
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-floating">
+                                <input type="text" name="longitude" class="form-control rounded-top" id="longitude" placeholder="longitude" hidden>
+                                <input type="text" name="latitude" class="form-control rounded-top" id="latitude" placeholder="Latitude" hidden>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <button class="w-100 btn btn-lg btn-secondary mt-3" type="submit">Atur</button>
-            </form>
-        </main>
+                    <div class="row justify-content-center">
+                        <div class="col-sm-6">
+                            <button class="w-100 btn btn-primary mt-3" type="submit">SET LOCATION</button>
+                        </div>
+                    </div>
+                </form>
+            </main>
+        </div>
     </div>
 </div>
 @endsection
@@ -34,13 +40,13 @@
     <script src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v5.0.0/mapbox-gl-geocoder.min.js"></script>
     <script>
         const defaultLocation = [97.1374572505087, 5.190902388476289]
-        
+
         mapboxgl.accessToken = '{{ env('MAPBOX_KEY') }}';
         const map = new mapboxgl.Map({
             container: 'map',
             style: 'mapbox://styles/mapbox/streets-v11', // light-v10, outdoors-v11, satellite-v9, streets-v11, dark-v10
             center: defaultLocation,
-            zoom: 9 
+            zoom: 9
         });
 
         map.addControl(
