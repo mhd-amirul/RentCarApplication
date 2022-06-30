b4556bcb512421bc8e157629cdb504cfb797427e
Your Name <mhd-amirul>
2022-06-30 13:20:47 +0700

    fix profile View

--- resources/views/pages/profile/profil.blade.php
+++ resources/views/pages/profile/profil.blade.php
@@ -1,80 +1,71 @@
 @extends('layouts.main')
 
 @section('container')
-<div class="mb-5">
-    {{-- @if (session()->has('success'))
-        <div class="alert alert-success alert-dismissible fade show" role="alert">
-            {{ session('success') }} 
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-    @endif --}}
-    <div class="row">
-        <div class="col-xl-12 mt-2">
-            <div class="card user-card-full">
-                    <div class="row m-l-0 m-r-0">
-                        <div class="col-sm-4 bg-c-lite-green user-profile">
-                            <div class="card-block text-center text-white">
-                                <img src="{{ isset($data->image) == null ? '' : asset('storage/' . $data->image) }}" alt="" class="img-fluid" width="100" style="clip-path: circle();">
-                                <h6 class="f-w-600"></h6>
-                            </div>
-                        </div>
-                        <div class="col-sm-8">
-                            <div class="card-block">
-                                <h6 class="m-b-20 p-b-5 b-b-default f-w-600">Information :</h6>
-                                <div class="row">
-                                    <div class="col-sm-6 mb-4">
-                                        <div class="form-floating">
-                                            <input type="text" class="form-control rounded-top" id="nm_pu" value="{{ $data->username }}">
-                                            <label for="nm_pu">Username</label>
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-6 mb-4">
-                                        <div class="form-floating">
-                                            <input type="text" class="form-control rounded-top" id="nm_pu" value="{{ $data->email }}">
-                                            <label for="nm_pu">Email</label>
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-6 mb-4">
-                                        <div class="form-floating">
-                                            <input type="text" class="form-control rounded-top" id="nm_pu" value="+62 {{ $data->no_hp }}">
-                                            <label for="nm_pu">No Hp</label>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-sm-9 mt-5 d-flex justify-content-end">
-                                                <a href="{{ route('profil.edit', $data->id) }}" class="btn btn-warning">
-                                                    <i class="bi bi-pencil-square"></i></i> Edit Profil
-                                                </a>
-                                                <a href="{{ route('changePass', $data->id) }}" class="ml-2 btn btn-warning">
-                                                    <i class="mr-2 bi bi-key"></i>Ganti Password
-                                                </a>
-                                                @if ($data->role === 'rental')
-                                                    <a href="{{ route('toko.index') }}" class="ml-2 btn btn-warning">
-                                                        <i class="mr-2 bi bi-house-fill"></i>Toko
-                                                    </a>
-                                                @endif
-                                                @if ($data->role === 'admin')
-                                                    <a href="{{ route('dashboard.index') }}" class="ml-2 btn btn-warning">
-                                                        <i class="mr-2 bi bi-shield-fill-exclamation"></i>Administrator
-                                                    </a>
-                                                @endif
-                                                @if ($data->role === 'user')
-                                                    @if ($makeshop)
-                                                        <a class="ml-2 btn btn-warning">
-                                                            <i class="mr-2 bi bi-house-fill"></i>Buka Toko
-                                                        </a>
-                                                    @else
-                                                        <a href="{{ route('profil.create')}}" class="ml-2 btn btn-warning">
-                                                            <i class="mr-2 bi bi-house-fill"></i>Buka Toko
-                                                        </a>
-                                                    @endif
-                                                @endif
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+{{-- @if (session()->has('success'))
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+        {{ session('success') }}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+@endif --}}
+<div class="row justify-content-center mt-5">
+    <div class="col-sm-10">
+        <div class="card mb-3">
+            <div class="bg-c-lite-green text-center">
+                <img src="{{ isset($data->image) == null ? url('images/person.png') : asset('storage/' . $data->image) }}" width="300" class="{{ isset($data->image) == null ? 'my-3' : '' }}" style="clip-path: circle()">
+            </div>
+
+            <div class="card-body">
+                <h4 class="card-title">INFORMASI AKUN</h4>
+                <hr class="m-0 p-0">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="form-floating mt-3">
+                            <input type="text" class="form-control rounded-top" id="nm_pu" value="{{ $data->username }}">
+                            <label for="nm_pu">Username</label>
                         </div>
                     </div>
+                    <div class="col-sm-6">
+                        <div class="form-floating mt-3">
+                            <input type="text" class="form-control rounded-top" id="nm_pu" value="+62 {{ $data->no_hp }}">
+                            <label for="nm_pu">No Hp</label>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="form-floating mt-3">
+                            <input type="text" class="form-control rounded-top" id="nm_pu" value="{{ $data->email }}">
+                            <label for="nm_pu">Email</label>
+                        </div>
+                    </div>
+                    <div class="row mt-4 mb-3">
+                        <div class="col-sm-10">
+                            <a href="{{ route('profil.edit', $data->id) }}" class="btn btn-warning ml-1">
+                                <i class="bi bi-pencil-square"></i></i> EDIT PROFIL
+                            </a>
+                            <a href="{{ route('changePass', $data->id) }}" class="ml-2 btn btn-warning">
+                                <i class="mr-2 bi bi-key"></i>RESET PASSWORD
+                            </a>
+                            @if ($data->role === 'rental')
+                                <a href="{{ route('toko.index') }}" class="ml-2 btn btn-warning">
+                                    <i class="mr-2 bi bi-house-fill"></i>TOKO
+                                </a>
+                            @elseif ($data->role === 'admin')
+                                <a href="{{ route('dashboard.index') }}" class="ml-2 btn btn-warning">
+                                    <i class="mr-2 bi bi-shield-fill-exclamation"></i>ADMINISTRATOR
+                                </a>
+                            @elseif ($data->role === 'user')
+                                @if ($makeshop)
+                                    <a class="ml-2 btn btn-warning">
+                                        <i class="mr-2 bi bi-house-fill"></i>ON PROCESS
+                                    </a>
+                                @else
+                                    <a href="{{ route('profil.create')}}" class="ml-2 btn btn-warning">
+                                        <i class="mr-2 bi bi-house-fill"></i>BUKA TOKO
+                                    </a>
+                                @endif
+                            @endif
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
