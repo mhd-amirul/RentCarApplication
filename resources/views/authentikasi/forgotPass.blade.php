c786a4ced1a9b2817d321e371c7bfee2c6392a76
Your Name <mhd-amirul>
2022-06-30 11:28:21 +0700

    fix forgotPass View

--- resources/views/authentikasi/forgotPass.blade.php
+++ resources/views/authentikasi/forgotPass.blade.php
@@ -2,25 +2,30 @@
 
 @section('container')
 <div class="container">
-    <div class="row">
-        <div class="col-md-4 offset-md-4 form" style="margin-bottom: 3cm; margin-top: 2cm;">
-            <form action="" method="POST" autocomplete="">
-                @csrf
-                <h2 class="text-center mt-5">Forgot Password</h2>
-                <p class="text-center">Enter your email address</p>
-                
-                <div class="form-floating">
-                    <input name="email" type="email" class="mb-3 form-control @error('email') is-invalid @enderror" id="email" placeholder="email" value="{{ old('email') }}" required>
-                    <label for="email">Email</label>
-                    @error('email')
-                        <div class="invalid-feedback mb-2 mt-2">
-                            {{ $message }}
-                        </div>
-                    @enderror
+    <div class="row justify-content-center">
+        <div class="col-lg-7" style="margin-bottom: 3cm; margin-top: 2cm;">
+            <div class="card px-3 py-3 border border-gray-800">
+                <div class="row justify-content-center mb-5">
+                    <div class="col-sm-7">
+                        <form action="" method="POST" autocomplete="">
+                            @csrf
+                            <h2 class="text-center mt-5">Forgot Password</h2>
+                            <p class="text-center">Enter your email address</p>
+
+                            <div class="form-floating">
+                                <input name="email" type="email" class="mb-3 form-control @error('email') is-invalid @enderror" id="email" placeholder="email" value="{{ old('email') }}" required>
+                                <label for="email">Email</label>
+                                @error('email')
+                                    <div class="invalid-feedback mb-2 mt-2">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+                            <button class="w-100 btn btn-primary" type="submit">SUBMIT</button>
+                        </form>
+                    </div>
                 </div>
-                
-                <button class="w-100 btn btn-lg btn-secondary" type="submit">Sign in</button>
-            </form>
+            </div>
         </div>
     </div>
 </div>
