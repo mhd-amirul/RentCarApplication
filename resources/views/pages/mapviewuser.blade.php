0cac0f6d3480d8845436c89c563e8423129bec7e
Your Name <mhd-amirul>
2022-06-30 14:20:16 +0700

    fix mapUser View

--- resources/views/pages/mapviewuser.blade.php
+++ resources/views/pages/mapviewuser.blade.php
@@ -2,31 +2,35 @@
 
 @section('container')
     <div class="row justify-content-center">
-        <div class="col-13">
-            <div class="row">
-                <div class="col">
-                    <input type="text" hidden name="longitude" id="longitude" value="{{ isset($shop->longitude) ? $shop->longitude : '97.1374572505087' }}" >
-                    <input type="text" hidden name="latitude" id="latitude" value="{{ isset($shop->latitude) ? $shop->latitude : '5.190902388476289' }}" >
-                    <input type="text" hidden name="nm_pu" id="nm_pu" value="{{ isset($shop->nm_pu) ? $shop->nm_pu : 'Empty...' }}" >
-                    <input type="text" hidden name="nm_usaha" id="nm_usaha" value="{{ isset($shop->nm_usaha) ? $shop->nm_usaha : 'Empty...' }}" >
-                    <input type="text" hidden name="no_hp" id="no_hp" value="{{ isset($shop->user->no_hp) ? $shop->user->no_hp : 'Empty...' }}" >
+        <div class="col-lg-12">
+            <div class="card px-5 pt-4 pb-5 border border-gray-800">
+                <div class="col-sm-12">
+                    <div class="row">
+                        <div class="col">
+                            <input type="text" hidden name="longitude" id="longitude" value="{{ isset($shop->longitude) ? $shop->longitude : '97.1374572505087' }}" >
+                            <input type="text" hidden name="latitude" id="latitude" value="{{ isset($shop->latitude) ? $shop->latitude : '5.190902388476289' }}" >
+                            <input type="text" hidden name="nm_pu" id="nm_pu" value="{{ isset($shop->nm_pu) ? $shop->nm_pu : 'Empty...' }}" >
+                            <input type="text" hidden name="nm_usaha" id="nm_usaha" value="{{ isset($shop->nm_usaha) ? $shop->nm_usaha : 'Empty...' }}" >
+                            <input type="text" hidden name="no_hp" id="no_hp" value="{{ isset($shop->user->no_hp) ? $shop->user->no_hp : 'Empty...' }}" >
+                        </div>
+                    </div>
+
+                    <div class="card-header bg-dark text-white mt-3 mb-3">
+                        Map
+                    </div>
+                    <div class="card_body border border-dark">
+                        <div id='map' style='width: 100%; height: 70vh;'></div>
+                    </div>
                 </div>
-            </div>
-    
-            <div class="card-header bg-dark text-white mt-3 mb-3">
-                Map
-            </div>
-            <div class="card_body border border-dark">
-                <div id='map' style='width: 100%; height: 70vh;'></div>  
             </div>
         </div>
     </div>
 @endsection
 
 @push('script-map-user')
-    <link 
-        rel="stylesheet" 
-        href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-directions/v4.1.0/mapbox-gl-directions.css" 
+    <link
+        rel="stylesheet"
+        href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-directions/v4.1.0/mapbox-gl-directions.css"
         type="text/css">
     <script src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-directions/v4.1.0/mapbox-gl-directions.js"></script>
     <script src='https://api.mapbox.com/mapbox-gl-js/v2.8.2/mapbox-gl.js'></script>
