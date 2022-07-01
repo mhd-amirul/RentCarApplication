86de4fe600f34d5b8a1ddb38e27f77d97a678479
Your Name <mhd-amirul>
2022-07-01 22:00:28 +0700

    fix dashboardAdmin view

--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -1,106 +1,106 @@
 @extends('layouts.main')
 
 @section('container')
-@if (session()->has('success'))
-<div class="alert alert-success alert-dismissible fade show" role="alert">
-    {{ session('success') }}
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-</div>
-@endif
-<h2 class="text-center">DASHBOARD ADMIN</h2><hr>
-    <div class="row">
-        <div class="col-sm-6 col-lg-3">
-            <div class="card text-white bg-primary">
-                <div class="card-body">
-                    <div class="pt-1">
-                        <a class="text-white fs-3" href="{{ route('allusers.index') }}">
-                            <i class="mr-2 bi bi-person-fill"> Pengguna</i>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!--/.col-->
-        <div class="col-sm-6 col-lg-3">
-            <div class="card text-white bg-danger">
-                <div class="card-body">
-                    <div class="pt-1">
-                        <a class="text-white fs-3" href="{{ route('allshops.index') }}">
-                            <i class="mr-2 bi bi-house-fill"> Toko</i>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!--/.col-->
-        <div class="col-sm-6 col-lg-3">
-            <div class="card text-white bg-warning">
-                <div class="card-body">
-                    <div class="pt-1">
-                        <a class="text-white fs-3" href="{{ route('allkriteria.index') }}">
-                            <i class="mr-2 bi bi-card-list"> Kriteria</i>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!--/.col-->
-        <div class="col-sm-6 col-lg-3">
-            <div class="card text-white bg-success">
-                <div class="card-body">
-                    <div class="pt-1">
-                        <a class="text-white fs-3" href="{{ route('allalternatif.index') }}">
-                            <i class="mr-2 bi bi-blockquote-right"> Alternatif</i>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-<div class="card mb-3 mt-2 bg-secondary text-white">
-    <div class="card-body p-2">
-        <h6 class="card-title m-0">Pendaftar Toko : </h5>
-    </div>
-</div>
-
-<form action="{{ route('dashboard.index') }}">
-    <div class="input-group mb-3">
-    <input type="text" class="form-control" placeholder="search.." name="searchms" value="{{ request('searchms') }}">
-    <button class="btn btn-secondary" type="submit">Search</button>
-</div>
-</form>
-
-@if ($makeshop->count())
-<div class="container">
-    <div class="row">
-        @foreach( $makeshop as $ms )
-            <div class="col-md-3 mb-3">
-                <div class="card">
-                    @if ($ms->pas_foto)
-                        <div style="max-height: 166px; overflow: hidden;" class="bg-dark">
-                            <img src="{{ asset('storage/' . $ms->pas_foto) }}" alt="" class="img-fluid">
-                        </div>
-                    @else
-                        <img src="{{ url('images/notfound.png') }}" alt="null" class="card-img-top">
-                    @endif
+<div class="row justify-content-center">
+    <div class="col-lg-12">
+        <div class="card px-5 py-5 border border-grey-500">
+            <h2 class="m-b-20 p-b-5 b-b-default mt-3 text-center">DASHBOARD ADMIN</h2>
+        <div class="row">
+            <div class="col-sm-6 col-lg-3">
+                <div class="card text-white bg-primary">
                     <div class="card-body">
-                        <p class="card-text">
-                            User ID : {{ $ms->user_id }} <br>
-                            Nama Pemilik : {{ $ms->nm_pu }} <br>
-                            Nama Usaha : {{ $ms->nm_usaha }} <br>
-                        </p>
-                        <a href="{{ route('dashboard.show',$ms->id) }}" class="btn-sm btn-primary">
-                            <i class="bi bi-eye-fill" style="color: rgb(0, 0, 0);"></i> Detail
-                        </a>
+                        <div class="pt-1">
+                            <a class="text-white fs-3" href="{{ route('allusers.index') }}">
+                                <i class="mr-2 bi bi-person-fill"> Pengguna</i>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
-        @endforeach
+            <div class="col-sm-6 col-lg-3">
+                <div class="card text-white bg-danger">
+                    <div class="card-body">
+                        <div class="pt-1">
+                            <a class="text-white fs-3" href="{{ route('allshops.index') }}">
+                                <i class="mr-2 bi bi-house-fill"> Toko</i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+                <div class="card text-white bg-warning">
+                    <div class="card-body">
+                        <div class="pt-1">
+                            <a class="text-white fs-3" href="{{ route('allkriteria.index') }}">
+                                <i class="mr-2 bi bi-card-list"> Kriteria</i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+                <div class="card text-white bg-success">
+                    <div class="card-body">
+                        <div class="pt-1">
+                            <a class="text-white fs-3" href="{{ route('allalternatif.index') }}">
+                                <i class="mr-2 bi bi-blockquote-right"> Alternatif</i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
     </div>
 </div>
-@else
-    <p class="text-center fs-4 mt-5 mb-5">Pencarian tidak ditemukan!</p>
-@endif
+<div class="row justify-content-center">
+    <div class="col-lg-12">
+        <div class="card px-5 py-5 border border-grey-500">
+            <div class="row justify-content-center">
+                <div class="col-sm-12">
+                    <h4 class="m-b-20 p-b-5 b-b-default mt-3">Pendaftar Toko : </h4>
+                    <form action="{{ route('dashboard.index') }}">
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="search.." name="searchms" value="{{ request('searchms') }}">
+                            <button class="btn btn-secondary" type="submit">Search</button>
+                        </div>
+                    </form>
+                </div>
+                <div class="col-lg-8">
+                    <table class="table table-responsive table-hover">
+                        <thead>
+                            <tr class="text-center">
+                                <th scope="col">User ID</th>
+                                <th scope="col">Nama Pemilik</th>
+                                <th scope="col">Nama Usaha</th>
+                                <th scope="col">NIK</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @forelse ($makeshop as $ms)
+                                <tr class="text-center">
+                                    <td>{{ $ms->user_id }}</td>
+                                    <td>{{ $ms->nm_pu }}</td>
+                                    <td>{{ $ms->nm_usaha }}</td>
+                                    <td>{{ $ms->nik }}</td>
+                                    <td>
+                                        <a href="{{ route('dashboard.show',$ms->id) }}" class="btn-sm btn-info"><i class="bi bi-eye-fill" style="color: rgb(0, 0, 0);"></i></a>
+                                    </td>
+                                </tr>
+                            @empty
+                                <tr>
+                                    <td colspan="7" class="text-center h3">DATA KOSONG</td>
+                                </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 <div class="panel">
     <div id="Chart-admin">
