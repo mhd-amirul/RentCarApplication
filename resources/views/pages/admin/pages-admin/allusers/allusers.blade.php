41e47d9f9705d0c92f80c3959336090017321199
Your Name <mhd-amirul>
2022-07-01 22:12:28 +0700

    fix UserAdmin view

--- resources/views/pages/admin/pages-admin/allusers/allusers.blade.php
+++ resources/views/pages/admin/pages-admin/allusers/allusers.blade.php
@@ -1,113 +1,123 @@
 @extends('layouts.main')
 
 @section('container')
-<div class="card mb-3 mt-2 bg-secondary text-white"><div class="card-body p-2"><h6 class="card-title m-0">Menu</h5></div></div>
 <div class="row justify-content-center">
-    <div class="col-lg-2">
-        <div class="card text-white bg-dark">
-            <div class="card-body">
-                <div class="pt-1">
-                    <a class="text-white fs-6" href="{{ route('dashboard.index') }}">
-                        <i class="mr-2 bi bi-house"> Dashboard</i>
-                    </a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-lg-2">
-        <div class="card text-white bg-secondary">
-            <div class="card-body">
-                <div class="pt-1">
-                    <a class="text-white fs-6" href="{{ route('allusers.create') }}">
-                        <i class="mr-2 bi bi-plus-circle"> Add User</i>
-                    </a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-lg-2">
-        <div class="card text-white bg-danger">
-            <div class="card-body">
-                <div class="pt-1">
-                    <a class="text-white fs-6" href="{{ route('allshops.index') }}">
-                        <i class="mr-2 bi bi-house-fill"> Toko</i>
-                    </a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-lg-2">
-        <div class="card text-white bg-warning">
-            <div class="card-body">
-                <div class="pt-1">
-                    <a class="text-white fs-6" href="{{ route('allkriteria.index') }}">
-                        <i class="mr-2 bi bi-card-list"> Kriteria</i>
-                    </a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-lg-2">
-        <div class="card text-white bg-success">
-            <div class="card-body">
-                <div class="pt-1">
-                    <a class="text-white fs-6" href="{{ route('allalternatif.index') }}">
-                        <i class="mr-2 bi bi-blockquote-right"> Alternatif</i>
-                    </a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="card mb-3 mt-2 bg-secondary text-white">
-    <div class="card-body p-2">
-        <h6 class="card-title m-0">Pengguna : </h5>
-    </div>
-</div>
-
-<form action="{{ route('allusers.index') }}">
-    <div class="input-group mb-3">
-    <input type="text" class="form-control" placeholder="search.." name="searchUser" value="{{ request('searchUser') }}">
-    <button class="btn btn-secondary" type="submit">Search</button>
-</div>
-</form>
-
-@if ($user->count())
-<div class="container">
-    <div class="row">
-        @foreach( $user as $user )
-            <div class="col-sm-3 mb-3">
-                <div class="card">
-                    @if ($user->image)
-                        <div style="max-height: 166px; overflow: hidden;" class="bg-dark">
-                            <img src="{{ asset('storage/' . $user->image) }}" alt="" class="img-fluid">
+    <div class="col-lg-12">
+        <div class="card px-4 py-4 border border-grey-500">
+            <h2 class="m-b-20 p-b-5 b-b-default mt-3 text-center">DASHBOARD ADMIN</h2>
+            <div class="row justify-content-center">
+                <div class="col-lg-2">
+                    <div class="card text-white bg-dark">
+                        <div class="card-body">
+                            <div class="pt-1">
+                                <a class="text-white fs-6" href="{{ route('dashboard.index') }}">
+                                    <i class="mr-2 bi bi-house"> Dashboard</i>
+                                </a>
+                            </div>
                         </div>
-                    @else
-                        <img src="{{ url('images/notfound.png') }}" alt="null" class="card-img-top">
-                    @endif
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $user->username }}</h5>
-
-                        <p class="card-text">
-                            ID : {{ $user->id }} <br> Email : {{ $user->email }} <br> Role : {{ $user->role }} <br> No Hp : {{ $user->no_hp }} <br> Created At : {{ $user->created_at }}
-                        </p>
-                        <a href="{{ route('allusers.show',$user->id) }}" class="btn-sm btn-primary"><i class="bi bi-eye-fill" style="color: rgb(0, 0, 0);"></i></a>
-                        <a href="{{ route('allusers.edit', $user->id) }}" class="btn-sm btn-warning"><i class="bi bi-pencil-square" style="color: rgb(0, 0, 0);"></i></a>
-
-                        <form action="{{ route('allusers.destroy', $user->id) }}" method="post" class="d-inline">
-                            @method('delete')
-                            @csrf
-                            <button class="btn-sm btn-danger border-0" style="color: rgb(0, 0, 0);" onclick="return confirm('Yakin Ingin Menghapus?')">
-                                <i class="bi bi-trash-fill"></i>
-                            </button>
-                        </form>
+                    </div>
+                </div>
+                <div class="col-lg-2">
+                    <div class="card text-white bg-secondary">
+                        <div class="card-body">
+                            <div class="pt-1">
+                                <a class="text-white fs-6" href="{{ route('allusers.create') }}">
+                                    <i class="mr-2 bi bi-plus-circle"> Add User</i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2">
+                    <div class="card text-white bg-danger">
+                        <div class="card-body">
+                            <div class="pt-1">
+                                <a class="text-white fs-6" href="{{ route('allshops.index') }}">
+                                    <i class="mr-2 bi bi-house-fill"> Toko</i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2">
+                    <div class="card text-white bg-warning">
+                        <div class="card-body">
+                            <div class="pt-1">
+                                <a class="text-white fs-6" href="{{ route('allkriteria.index') }}">
+                                    <i class="mr-2 bi bi-card-list"> Kriteria</i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2">
+                    <div class="card text-white bg-success">
+                        <div class="card-body">
+                            <div class="pt-1">
+                                <a class="text-white fs-6" href="{{ route('allalternatif.index') }}">
+                                    <i class="mr-2 bi bi-blockquote-right"> Alternatif</i>
+                                </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        @endforeach
+        </div>
     </div>
 </div>
-@else
-    <p class="text-center fs-4 mt-5 mb-5">Data tidak ditemukan!</p>
-@endif
+<div class="row justify-content-center">
+    <div class="col-lg-12">
+        <div class="card px-5 py-5 border border-grey-500">
+            <div class="row justify-content-center">
+                <div class="col-sm-12">
+                    <h4 class="m-b-20 p-b-5 b-b-default mt-3">Pendaftar Toko : </h4>
+                    <form action="{{ route('allusers.index') }}">
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="search.." name="searchUser" value="{{ request('searchUser') }}">
+                            <button class="btn btn-secondary" type="submit">Search</button>
+                        </div>
+                    </form>
+                </div>
+                <div class="col-lg-8">
+                    <table class="table table-responsive table-hover">
+                        <thead>
+                            <tr class="text-center">
+                                <th scope="col">Username</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Role</th>
+                                <th scope="col">No Hp</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @forelse ($user as $user)
+                                <tr class="text-center">
+                                    <td>{{ $user->username }}</td>
+                                    <td>{{ $user->email }}</td>
+                                    <td>{{ $user->role }}</td>
+                                    <td>0{{ $user->no_hp }}</td>
+                                    <td>
+                                        <a href="{{ route('allusers.show',$user->id) }}" class="btn-sm btn-primary"><i class="bi bi-eye-fill" style="color: rgb(0, 0, 0);"></i></a>
+                                        <a href="{{ route('allusers.edit', $user->id) }}" class="btn-sm btn-warning"><i class="bi bi-pencil-square" style="color: rgb(0, 0, 0);"></i></a>
+                                        <form action="{{ route('allusers.destroy', $user->id) }}" method="post" class="d-inline">
+                                            @method('delete')
+                                            @csrf
+                                            <button class="btn-sm btn-danger border-0" style="color: rgb(0, 0, 0);" onclick="return confirm('Yakin Ingin Menghapus?')">
+                                                <i class="bi bi-trash-fill"></i>
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @empty
+                                <tr>
+                                    <td colspan="7" class="text-center h3">Data tidak ditemukan!</td>
+                                </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
