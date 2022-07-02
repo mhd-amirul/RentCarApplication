fed6bbb5913e1c02a555a8745346ff589f9f8269
Your Name <mhd-amirul>
2022-07-02 09:05:24 +0700

    fix hasilRekom view

--- resources/views/pages/hasil.blade.php
+++ resources/views/pages/hasil.blade.php
@@ -8,27 +8,27 @@
     <h1></h1>
 </div>
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row justify-content-start">
         @forelse( $cars as $car )
             @if ($loop->first)
             <div class="row justify-content-center">
-                <div class="col-sm-12">
+                <div class="col-sm-12 ml-4">
                     <div class="card mb-3 text-center">
                         <div style="max-height: 350px; overflow: hidden;" class="bg-dark">
                             <img src="{{ isset($car->gambar1) == null ? url('images/notfound.png') : asset('storage/' . $car->gambar1) }}" alt="" class="img-fluid">
                         </div>
 
                         <div class="card-body">
-                            <h3 class="card-title">{{ $car->merk->nama }}</h3>
-                            <h4 class="card-title">Rank : {{ $loop->iteration }}</h4>
+                            <h4 class="card-title">Ranking {{ $loop->iteration }}</h4>
+                            <h3 class="card-title">Merk : {{ $car->merk->nama }}</h3>
                             <p>
                                 <small>
-                                    By : <a href="{{ route('profileToko', $car->shop->id) }}" class="text-decoration-none">{{ $car->shop->nm_usaha }}</a> {{ $car->created_at->diffForHumans() }}
+                                    <a href="{{ route('profileToko', $car->shop->id) }}" class="text-decoration-none">{{ $car->shop->nm_usaha }}</a> {{ $car->created_at->diffForHumans() }}
                                 </small>
                             </p>
                             <div class="row justify-content-center">
                                 <div class="col-sm-5">
-                                    <p class="card-text">Merk : {{ $car->merk->nama }}, Tahun Produksi : {{ $car->tahun_produksi->nama }}, Muatan Penumpang : {{ $car->muatan_penumpang->nama }}, Harga Sewa : {{ $car->harga_sewa->nama }}</p>
+                                    <p class="card-text">Tahun Produksi : {{ $car->tahun_produksi->nama }}, Muatan Penumpang : {{ $car->muatan_penumpang->nama }}, Harga Sewa : {{ $car->harga_sewa->nama }}</p>
                                     <a href="{{ route('detailMobil', $car->id) }}" class="btn btn-sm btn-primary w-100">Detail</a>
                                 </div>
                             </div>
@@ -41,15 +41,15 @@
                     <div class="card">
                         <img src="{{ isset($car->gambar1) == null ? url('images/notfound.png') : asset('storage/' . $car->gambar1) }}" alt="{{ $car->merk->nama }}" class="card-img-top">
                         <div class="card-body">
-                            <h4 class="card-title">{{ $car->merk->nama }}</h4>
-                            <h5 class="card-title">Rank : {{ $loop->iteration }}</h5>
+                            <h5 class="card-title">Ranking {{ $loop->iteration }}</h5>
+                            <h4 class="card-title">Merk : {{ $car->merk->nama }}</h4>
                             <p>
                                 <small>
-                                    Usaha : <a href="{{ route('profileToko', $car->shop->id) }}" class="text-decoration-none">{{ $car->shop->nm_usaha }}</a>
+                                    <a href="{{ route('profileToko', $car->shop->id) }}" class="text-decoration-none">{{ $car->shop->nm_usaha }}</a>
                                     {{ $car->created_at->diffForHumans() }}
                                 </small>
                             </p>
-                            <p class="card-text">Merk : {{ $car->merk->nama }}, Tahun Produksi : {{ $car->tahun_produksi->nama }}, Muatan Penumpang : {{ $car->muatan_penumpang->nama }}, Harga Sewa : {{ $car->harga_sewa->nama }}</p>
+                            <p class="card-text">Tahun Produksi : {{ $car->tahun_produksi->nama }}, Muatan Penumpang : {{ $car->muatan_penumpang->nama }}, Harga Sewa : {{ $car->harga_sewa->nama }}</p>
                             <a href="{{ route('detailMobil', $car->id) }}" class="btn btn-sm btn-primary">Detail</a>
                         </div>
                     </div>
