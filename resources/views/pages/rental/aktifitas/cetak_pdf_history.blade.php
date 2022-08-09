9d42ec6cb0d68303e4f05a5e75fa429cac178631
mhd-amirul <amirulm236@gmail.com>
2022-08-09 01:50:29 -0700

    Laporan Rekap

--- resources/views/pages/rental/aktifitas/cetak_pdf_history.blade.php
+++ resources/views/pages/rental/aktifitas/cetak_pdf_history.blade.php
@@ -3,151 +3,111 @@
         <title>Laporan Rental Mobil</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
         <style>
-            html {
-                font-size: 8pt !important;
+            h3 {
+                font-size: 14pt;
             }
             body {
-                width: 230mm;
+                width: 100%;
                 height: 100%;
-                margin: 0 auto;
                 padding: 0;
-                font-size: 12pt;
                 background: rgb(204,204,204);
             }
             * {
                 box-sizing: border-box;
                 -moz-box-sizing: border-box;
             }
+            table {
+                font-size: 8pt;
+                margin: 0.5cm;
+                margin-top: 0;
+            }
 
             .main-page {
-                width: 210mm;
-                min-height: 297mm;
-                margin: 10mm auto;
+                width: 100%;
+                min-height: 100%;
                 background: white;
                 box-shadow: 0 0 0.5cm rgba(0,0,0,0.5);
             }
             .sub-page {
-                padding: 2cm;
-                height: 297mm;
+                padding: 0.3cm;
+                height: 210mm;
             }
             @page {
-                size: A4;
-                margin: 0;
+                size: landscape;
+                margin-top: 1cm;
+                margin-bottom: 0;
+                margin-left: 0;
+                margin-right: 0;
             }
             @media print {
                 html, body {
-                    width: 210mm;
-                    height: 297mm;
+                    width: 297mm;
+                    height: 210mm;
                 }
                 .main-page {
-                    margin: 0;
+                    margin: 1cm;
                     border: initial;
                     border-radius: initial;
                     width: initial;
                     min-height: initial;
                     box-shadow: initial;
                     background: initial;
+                    page-break-inside: auto;
                     page-break-after: always;
                 }
             }
         </style>
     </head>
-    <body>
-        @foreach ($histories as $history)
+    <body style="background: rgb(204,204,204);">
         <div class="main-page">
             <div class="sub-page">
                 <h3 align='center'>LAPORAN PEMINJAMAN MOBIL RENTAL</h3>
-                <div class="row mt-5 justify-content-center my-5">
+
+                <div class="row mt-3">
                     <table class="table table-bordered">
-                            <thead>
+                        <thead class="text-center">
                                 <tr>
                                     <th>NO.</th>
-                                    <th>NAMA PEMINJAM</th>
-                                    <th>NIK PEMINJAM</th>
-                                    <th>TANGGAL PEMINJAMAN</th>
-                                    <th>BATAS TANGGAL</th>
-                                    <th>STATUS</th>
-                                    <th>WAKTU PEMINAJAMAN</th>
+                                    <th>NAMA</th>
+                                    <th>NIK</th>
+                                    <th>NO. POL</th>
+                                    <th>MERK</th>
+                                    <th>TAHUN</th>
+                                    <th>CC</th>
+                                    <th>HARGA</th>
+                                    <th>TOKO</th>
+                                    <th>TGL PINJAM</th>
+                                    <th>TGL BATAS</th>
+                                    <th>SISA WAKTU</th>
                                 </tr>
                             </thead>
-                            <tbody>
+                            @foreach ($histories as $history)
+                            <tbody class="text-left">
                                 <tr>
                                     <th>{{ $loop->iteration }}</th>
                                     <td>{{ $history->nama_pinjam }}</td>
                                     <td>{{ $history->nik_pinjam }}</td>
-                                    <td>{{ $history->tgl_pinjam }}</td>
-                                    <td>{{ $history->batas_pinjam }}</td>
-                                    <td>{{ $history->status }}</td>
-                                    <td>0</td>
-                                </tr>
-                                <tr>
-                                    <td></td>
-                                    <td colspan="2">
-                                        <div style="max-height: 200px; max-width: 300px; overflow: hidden;" class="border border-dark">
-                                            <img src="{{ isset($history->sim_peminjam) == null ? url('images/notfound.png') : asset('storage/' . $history->sim_peminjam) }}" alt="#" class="img-fluid">
-                                        </div>
-                                    </td>
-                                    <td colspan="2">
-                                        <div style="max-height: 200px; max-width: 300px; overflow: hidden;" class="border border-dark">
-                                            <img src="{{ isset($history->ktp_peminjam) == null ? url('images/notfound.png') : asset('storage/' . $history->ktp_peminjam) }}" alt="#" class="img-fluid">
-                                        </div>
-                                    </td>
-                                    <td colspan="2">
-                                        <div style="max-height: 200px; max-width: 300px; overflow: hidden;" class="border border-dark">
-                                            <img src="{{ isset($history->foto_peminjam) == null ? url('images/notfound.png') : asset('storage/' . $history->foto_peminjam) }}" alt="#" class="img-fluid">
-                                        </div>
-                                    </td>
-                                </tr>
-                            </tbody>
-                            <thead>
-                                <tr>
-                                    <th></th>
-                                    <th>MERK</th>
-                                    <th>TAHUN PRODUKSI</th>
-                                    <th>HARGA SEWA</th>
-                                    <th>NO POLISI</th>
-                                    <th>ID</th>
-                                    <th>TOKO</th>
-                                </tr>
-                            </thead>
-                            @foreach ($cars as $car)
-                                @if ($car->id == $history->car_id)
-                                    <tbody>
-                                        <tr>
-                                            <td></td>
+                                    @foreach ($cars as $car)
+                                        @if ($car->id == $history->car_id)
+                                        <td>{{ $car->no_polisi }}</td>
                                             <td>{{ $car->merk->nama }}</td>
                                             <td>{{ $car->tahun_produksi->nama }}</td>
-                                            <td>{{ $car->harga_sewa->nama }}</td>
-                                            <td>{{ $car->no_polisi }}</td>
-                                            <td>{{ $car->id }}</td>
+                                            <td>{{ $car->kapasitas_mesin->nama }}</td>
+                                            <td>{{ number_format($car->harga_sewa->nama,2,',','.') }}</td>
                                             <td>{{ $car->shop->nm_usaha }}</td>
-                                        </tr>
-                                        <tr>
-                                            <td></td>
-                                            <td colspan="2">
-                                                <div style="max-height: 200px; max-width: 300px; overflow: hidden;" class="border border-dark">
-                                                    <img src="{{ isset($car->gambar1) == null ? url('images/notfound.png') : asset('storage/' . $car->gambar1) }}" alt="#" class="img-fluid">
-                                                </div>
-                                            </td>
-                                            <td colspan="2">
-                                                <div style="max-height: 200px; max-width: 300px; overflow: hidden;" class="border border-dark">
-                                                    <img src="{{ isset($car->gambar2) == null ? url('images/notfound.png') : asset('storage/' . $car->gambar2) }}" alt="#" class="img-fluid">
-                                                </div>
-                                            </td>
-                                            <td colspan="2">
-                                                <div style="max-height: 200px; max-width: 300px; overflow: hidden;" class="border border-dark">
-                                                    <img src="{{ isset($car->gambar3) == null ? url('images/notfound.png') : asset('storage/' . $car->gambar3) }}" alt="#" class="img-fluid">
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                @endif
-                            @endforeach
+                                        @endif
+                                    @endforeach
+                                    <td>{{ $history->tgl_pinjam }}</td>
+                                    <td>{{ $history->batas_pinjam }}</td>
+                                    <td>{{ $history->sisa_waktu != null ? $history->sisa_waktu : '0' }}</td>
+                                </tr>
+                                @endforeach
+                            </tbody>
                     </table>
                 </div>
+
             </div>
         </div>
-        @endforeach
 
         <script>
             window.print();
