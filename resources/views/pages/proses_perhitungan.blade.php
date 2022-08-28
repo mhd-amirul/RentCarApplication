8f24c03f9b4159388fe95f9916f410f2d8d8b44b
Your Name <mhd-amirul>
2022-08-28 14:11:17 +0700

    revision

--- resources/views/pages/proses_perhitungan.blade.php
+++ resources/views/pages/proses_perhitungan.blade.php
@@ -8,7 +8,7 @@
         </div>
     </div> --}}
     <div class="col-lg-12 justify-content-center">
-        {{-- <div class="row">
+        <div class="row">
             <div class="col-lg-12">
                 <div class="card px-5 pt-5">
                     <h4 class="m-b-20 p-b-5 b-b-default f-w-600">Pengambilan data kriteria dan alternatif dari database</h4>
@@ -137,7 +137,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-sm-6">
+            <div class="col-sm-12">
                 <div class="card px-5 py-5">
                     <h3>Normalisasi</h3>
                     <table class="table table-responsive table-hover text-center">
@@ -162,6 +162,20 @@
                     </table>
                 </div>
             </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card px-5 pt-5">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <h4 class="m-b-20 p-b-5 b-b-default f-w-600">Hasil Optimasi</h4>
+                        </div>
+                        <div class="col-sm-6">
+                            <h4 class="m-b-20 p-b-5 b-b-default f-w-600">Perangkingan</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div class="col-sm-6">
                 <div class="card px-5 py-5">
                     <h3>Optimasi</h3>
@@ -181,13 +195,6 @@
                             @endforeach
                         </tbody>
                     </table>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="card px-5 pt-5">
-                    <h4 class="m-b-20 p-b-5 b-b-default f-w-600">Perangkingan di urutkan dari nilai terbesar ke terkecil</h4>
                 </div>
             </div>
             <div class="col-sm-6">
@@ -213,8 +220,8 @@
                     </table>
                 </div>
             </div>
-        </div> --}}
-        <div class="row">
+        </div>
+        {{-- <div class="row">
             <div class="col-lg-12">
                 <div class="card px-5 pt-5">
                     <h4 class="m-b-20 p-b-5 b-b-default f-w-600">Hasil rekomendasi mobil rental</h4>
@@ -227,7 +234,7 @@
                             <tr>
                                 <th scope="col">Ranking</th>
                                 <th scope="col">ID</th>
-                                {{-- <th scope="col">No Polisi</th> --}}
+                                <th scope="col">No Polisi</th>
                                 <th scope="col">Merk</th>
                                 <th scope="col">Tahun</th>
                                 <th scope="col">Konfisi Fisik</th>
@@ -236,8 +243,8 @@
                                 <th scope="col">CC</th>
                                 <th scope="col">Jenis BBM</th>
                                 <th scope="col">Harga Sewa</th>
-                                {{-- <th scope="col">Stok</th> --}}
-                                {{-- <th scope="col">Action</th> --}}
+                                <th scope="col">Stok</th>
+                                <th scope="col">Action</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -245,7 +252,7 @@
                                 <tr>
                                     <th scope="row">{{ $car->ranking }}</th>
                                     <td>{{ $car->id }}</td>
-                                    {{-- <td>{{ $car->no_polisi }}</td> --}}
+                                    <td>{{ $car->no_polisi }}</td>
                                     <td>{{ $car->merk->nama }}</td>
                                     <td>{{ $car->tahun_produksi->nama }}</td>
                                     <td>{{ $car->kondisi_fisik->nama }}</td>
@@ -254,12 +261,12 @@
                                     <td>{{ $car->kapasitas_mesin->nama }}</td>
                                     <td>{{ $car->jenis_bbm->nama }}</td>
                                     <td>{{ "Rp. " . number_format($car->harga_sewa->nama,2,',','.') . '\hari' }}</td>
-                                    {{-- <td>{{ $car->stok }}</td> --}}
-                                    {{-- <td>
+                                    <td>{{ $car->stok }}</td>
+                                    <td>
                                         <a href="{{ route('detailMobil', $car->slug) }}" class="btn btn-sm btn-info">
                                             <i class="bi bi-eye-fill" style="color: rgb(0, 0, 0);"></i>
                                         </a>
-                                    </td> --}}
+                                    </td>
                                 </tr>
                             @endforeach
                         </tbody>
@@ -267,7 +274,249 @@
                 </div>
             </div>
 
+        </div> --}}
+        <div class="row justify-content-start">
+            <div class="col-lg-12 mt-4">
+                <div class="card px-5 pt-5">
+                    <h4 class="m-b-20 p-b-5 b-b-default f-w-600 text-center">HASIL REKOMENDASI</h4>
+                </div>
+            </div>
+            @if ($cars)
+                <div class="col-lg-6">
+                    <div class="card pt-3 pb-3">
+                        <div id="container" class="mt-5 mx-3"></div>
+                    </div>
+                </div>
+            @else
+            @endif
+            @forelse( $cars as $car )
+                @if ($car->ranking == 1)
+                    <div class="col-sm-6">
+                        <div class="card mb-3 text-center">
+                            <div style="max-height: 350px; overflow: hidden;" class="bg-dark">
+                                <img src="{{ isset($car->gambar1) == null ? url('images/notfound.png') : asset('storage/' . $car->gambar1) }}" alt="" class="img-fluid">
+                            </div>
+
+                            <div class="card-body">
+                                <h4 class="card-title" id="rank{{ $car->ranking }}">Rank. {{ $car->ranking }}</h4>
+                                <h3 class="card-title">Merk : {{ $car->merk->nama }}</h3>
+                                <p>
+                                    <small>
+                                        <a href="{{ route('profileToko', $car->shop->slug) }}" class="text-decoration-none">{{ $car->shop->nm_usaha }}</a> {{ $car->created_at->diffForHumans() }}
+                                    </small>
+                                </p>
+                                <div class="row justify-content-center">
+                                    <div class="col-sm-5">
+                                        <p class="card-text">Tahun Produksi : {{ $car->tahun_produksi->nama }}, Muatan Penumpang : {{ $car->muatan_penumpang->nama }}, Harga Sewa : {{ "Rp. " . number_format($car->harga_sewa->nama,2,',','.') }}/hari</p>
+                                        <a href="{{ route('detailMobil', $car->slug) }}" class="btn btn-sm btn-primary w-100">Detail</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endif
+            @empty
+                <h1 class="text-center mt-2" style="margin-bottom: 7cm">MOBIL TIDAK DITEMUKAN</h1>
+            @endforelse
+            <div class="row justify-content-center ml-1">
+                @forelse ($cars as $car)
+                    @if ($car->ranking != 1)
+                        <div class="col-sm-4 mb-3">
+                            <div class="card">
+                                <img src="{{ isset($car->gambar1) == null ? url('images/notfound.png') : asset('storage/' . $car->gambar1) }}" alt="{{ $car->merk->nama }}" class="card-img-top">
+                                <div class="card-body">
+                                    <h5 class="card-title">Rank. {{ $car->ranking }}</h5>
+                                    <h4 class="card-title">Merk : {{ $car->merk->nama }}</h4>
+                                    <p>
+                                        <small>
+                                            <a href="{{ route('profileToko', $car->shop->slug) }}" class="text-decoration-none">{{ $car->shop->nm_usaha }}</a>
+                                            {{ $car->created_at->diffForHumans() }}
+                                        </small>
+                                    </p>
+                                    <p class="card-text">Tahun Produksi : {{ $car->tahun_produksi->nama }}, Muatan Penumpang : {{ $car->muatan_penumpang->nama }}, Harga Sewa : {{ "Rp. " . number_format($car->harga_sewa->nama,2,',','.') }}</p>
+                                    <a href="{{ route('detailMobil', $car->slug) }}" class="btn btn-sm btn-primary">Detail</a>
+                                </div>
+                            </div>
+                        </div>
+                    @endif
+                @empty
+                    <p></p>
+                @endforelse
+            </div>
         </div>
     </div>
 </div>
 @endsection
+
+@push('trix')
+    <style>
+        #container {
+            height: 500px;
+        }
+
+        .highcharts-figure,
+        .highcharts-data-table table {
+            min-width: 320px;
+            max-width: 700px;
+            /* margin: 1em auto; */
+        }
+
+        .highcharts-data-table table {
+            font-family: Verdana, sans-serif;
+            border-collapse: collapse;
+            border: 1px solid #ebebeb;
+            margin: 10px auto;
+            text-align: center;
+            width: 100%;
+            max-width: 500px;
+        }
+
+        .highcharts-data-table caption {
+            padding: 1em 0;
+            font-size: 1.2em;
+            color: #555;
+        }
+
+        .highcharts-data-table th {
+            font-weight: 600;
+            padding: 0.5em;
+        }
+
+        .highcharts-data-table td,
+        .highcharts-data-table th,
+        .highcharts-data-table caption {
+            padding: 0.5em;
+        }
+
+        .highcharts-data-table thead tr,
+        .highcharts-data-table tr:nth-child(even) {
+            background: #f8f8f8;
+        }
+
+        .highcharts-data-table tr:hover {
+            background: #f1f7ff;
+        }
+    </style>
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script src="https://code.highcharts.com/modules/variable-pie.js"></script>
+    <script src="https://code.highcharts.com/modules/exporting.js"></script>
+    <script src="https://code.highcharts.com/modules/export-data.js"></script>
+    <script src="https://code.highcharts.com/modules/accessibility.js"></script>
+
+@endpush
+
+@push('script-map')
+    <script type="text/javascript">
+        const array = {!! json_encode($cars) !!}
+        var data = [], len = array.length, i = 0;
+        console.log(array)
+        for(i;i<len;i++){
+            data.push({
+                name: array[i].merk.nama,
+                rank: "Rank. " + array[i].ranking,
+                tahun: array[i].tahun_produksi.nama,
+                seater: array[i].muatan_penumpang.nama,
+                harga: array[i].harga_sewa.nama,
+                // nilai akhir
+                y: array[i].nilai,
+                z: array[i].nilai
+            });
+        }
+
+        $(function() {
+            Highcharts.chart('container', {
+                chart: {
+                    type: 'variablepie'
+                },
+                title: {
+                    text: 'Chart Hasil Rekomendasi Mobil Rental'
+                },
+                tooltip: {
+                    headerFormat: '',
+                    pointFormat: '<span style="color:{point.color}">\u25CF</span> <b> {point.name}</b><br/>' +
+                        'Rank : {point.rank}<br/>' +
+                        'Tahun : {point.tahun}<br/>' +
+                        'Seater : {point.seater}<br/>' +
+                        'Harga : {point.harga}<br/>' +
+                        'Rasio : {point.y}<br/>'
+                        // 'Nilai  : {point.z}<br/>'
+                },
+                series: [{
+                    minPointSize: 10,
+                    innerSize: '20%',
+                    zMin: 0,
+                    name: 'countries',
+                    data: data
+                    // [
+                    //     {
+                    //         name: 'Rank. 1 : Avanza',
+                    //         tahun: 2020,
+                    //         seater:7,
+                    //         harga:400000,
+                    //         // nilai akhir
+                    //         y: 505370,
+                    //         z: 92.9
+                    //     }
+                    // ]
+                }]
+            });
+        });
+    </script>
+    {{-- <script>
+        const array = {!! json_encode($cars) !!}
+        var data = [], len = array.length, i = 0;
+        console.log(array)
+        for(i;i<len;i++){
+            data.push({
+                name : "Data"+i,
+                data : [{
+                    name: "Rank. " + array[i].ranking + " : " + array[i].merk.nama,
+                    tahun: array[i].tahun_produksi.nama,
+                    seater: array[i].muatan_penumpang.nama,
+                    harga: array[i].harga_sewa.nama,
+                    // nilai akhir
+                    y: array[i].nilai,
+                    z: array[i].nilai
+                }]
+            });
+        }
+        Highcharts.chart('container', {
+                chart: {
+                    type: 'column'
+                },
+                title: {
+                    text: 'Chart Hasil Rekomendasi Mobil Rental'
+                },
+                subtitle: {
+                    text: 'Source: CarRental.com'
+                },
+                xAxis: {
+                    categories: [
+                        'Jumlah Data'
+                    ],
+                    crosshair: true
+                },
+                yAxis: {
+                    min: 0,
+                    title: {
+                        text: 'Jumlah Data'
+                    }
+                },
+                tooltip: {
+                    headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                    pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                        '<td style="padding:0"><b>{point.y:.1f} </b></td></tr>',
+                    footerFormat: '</table>',
+                    shared: true,
+                    useHTML: true
+                },
+                plotOptions: {
+                    column: {
+                        pointPadding: 0.2,
+                        borderWidth: 0
+                    }
+                },
+                series: data
+        });
+    </script> --}}
+@endpush
